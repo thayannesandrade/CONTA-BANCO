348a04c2df21d6354d28ffeb38bad0460117f319
thayannesandrade <thayannesandrade@gmail.com>
2024-05-24 17:41:03 -0300

    resolvendo desafio: simulando uma conta bancária

---
+++ src/ContaTerminal.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        String agencia, nomeCliente;
+        int numeroConta;
+        float saldoConta;
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o seu nome:");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da agência:");
+        agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da conta:");
+        numeroConta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo em sua conta:");
+        saldoConta = scanner.nextFloat();
+        scanner.nextLine();
+
+        System.out.println("Olá " + nomeCliente + ",obrigado por criar uma conta em nosso banco. Sua agência é: " + agencia + ", o número da sua conta é: " + numeroConta + " e seu saldo é de R$: " + String.format("%.2f",saldoConta) + " e já se encontra disponível para saque");
+
+        scanner.close();
+    }
+}
